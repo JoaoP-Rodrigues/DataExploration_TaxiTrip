eb7309fe0e6c768922bc52306f134e3527f171c0
JoaoP-Rodrigues <joao.tribo@gmail.com>
2021-10-16 16:47:41 -0300

    Create third and fourth functions to calculate datas froms files Initiate the create charts

--- dataSprintTest.py
+++ dataSprintTest.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import json
 from functions import *
 
-#file = 'dataFiles/data-sample_data-nyctaxi-trips-2010-json_corrigido.json'
+#tripsFile = 'dataFiles/data-sample_data-nyctaxi-trips-2009-json_corrigido.json'
 
 tripsFile = 'dataFiles/teste.json'
 dfDataTrips = pd.read_json(tripsFile)
@@ -15,4 +15,12 @@ dfDataVendors = pd.read_csv(vendorsFile)
 
 #meanDistance(dfDataTrips)
 
-bigVendors(dfDataVendors, dfDataTrips)
+#bigVendors(dfDataVendors, dfDataTrips)
+
+#histogramCash(dfDataTrips)
+
+#tripsFile2012 = 'dataFiles/teste_2012.json'
+tripsFile2012 = 'dataFiles/data-sample_data-nyctaxi-trips-2010-json_corrigido.json'
+
+dfDataTrips12 = pd.read_json(tripsFile2012)
+timeSeries(dfDataTrips12)
